6f37490892b9b2e6b0b22db88b83761b6adaa032
Swathi <swathi.h@perfios.com>
2021-09-01 20:29:38 +0530

    MODIFIED: Adding multiple element at once

--- src/com/bridgelabz/binarysearchtree/BinarySearchTreeMain.java
+++ src/com/bridgelabz/binarysearchtree/BinarySearchTreeMain.java
@@ -3,9 +3,10 @@ package com.bridgelabz.binarysearchtree;
 public class BinarySearchTreeMain {
 	public static void main(String[] args) {
 		MyBinarySearchTree<Integer> myBinarySearchTree = new MyBinarySearchTree<Integer>();
-		myBinarySearchTree.add(56);
-		myBinarySearchTree.add(30);
-		myBinarySearchTree.add(70);
+		int [] array= {56, 30, 22, 11, 3, 16, 40, 70, 60, 95, 65, 63, 67};
+		for(int eachElement:array) {
+			myBinarySearchTree.add(eachElement);
+		}
 		int sizeOfTree = myBinarySearchTree.getSize();
 		System.out.println("Size of the Binary search tree is: " + sizeOfTree);
 
