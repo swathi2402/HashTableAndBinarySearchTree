caa2adc837e6df09df71ab1dc545e9affbf5d0d0
Swathi <swathi.h@perfios.com>
2021-09-01 20:23:52 +0530

    ADDED: Created method to add element to BST using recursion

--- src/com/bridgelabz/binarysearchtree/BinarySearchTreeMain.java
+++ src/com/bridgelabz/binarysearchtree/BinarySearchTreeMain.java
@@ -1,5 +1,13 @@
 package com.bridgelabz.binarysearchtree;
 
 public class BinarySearchTreeMain {
+	public static void main(String[] args) {
+		MyBinarySearchTree<Integer> myBinarySearchTree = new MyBinarySearchTree<Integer>();
+		myBinarySearchTree.add(56);
+		myBinarySearchTree.add(30);
+		myBinarySearchTree.add(70);
+		int sizeOfTree = myBinarySearchTree.getSize();
+		System.out.println("Size of the Binary search tree is: " + sizeOfTree);
 
+	}
 }
--- src/com/bridgelabz/binarysearchtree/MyBinarySearchTree.java
+++ src/com/bridgelabz/binarysearchtree/MyBinarySearchTree.java
@@ -1,5 +1,33 @@
 package com.bridgelabz.binarysearchtree;
 
-public class MyBinarySearchTree<T> {
+public class MyBinarySearchTree<K extends Comparable<K>> {
+	private MyBinaryNode<K> root;
+
+	public void add(K key) {
+		this.root = this.addRecursively(root, key);
+	}
+
+	private MyBinaryNode<K> addRecursively(MyBinaryNode<K> currentNode, K key) {
+		if (currentNode == null) {
+			return new MyBinaryNode<>(key);
+		}
+		int compareResult = key.compareTo(currentNode.key);
+		if (compareResult == 0)
+			return currentNode;
+		if (compareResult < 0) {
+			currentNode.left = addRecursively(currentNode.left, key);
+		} else {
+			currentNode.right = addRecursively(currentNode.right, key);
+		}
+		return currentNode;
+	}
+
+	public int getSize() {
+		return getSizeRecursively(root);
+	}
+
+	private int getSizeRecursively(MyBinaryNode<K> current) {
+		return (current == null) ? 0 : 1 + this.getSizeRecursively(current.left) + this.getSizeRecursively(current.right);
+	}
 
 }
